308fff2329308b71362726a8db9f313952e354b6
Meitar <meitargo@post.bgu.ac.il>
2020-11-10 14:29:30 +0200

    Initial upload

--- src/Session.cpp
+++ src/Session.cpp
@@ -2,3 +2,21 @@
 // Created by spl211 on 08/11/2020.
 //
 
+#include <iostream>
+#include <fstream>
+#include "json.hpp"
+#include "Session.h"
+
+using json = nlohmann::json;
+using namespace std;
+
+Session::Session(const std::string &path) {
+    ifstream i(path);
+    json j;
+    j << i;
+    g=Graph(j["graph"]);
+    treeType= j["tree"];
+
+
+};
+
