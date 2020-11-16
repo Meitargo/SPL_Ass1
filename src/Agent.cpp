6469c877a2b6eb7c78a35f16a03d3d85b958097e
Nitzan <adamnitza@post.bgu.ac.il>
2020-11-16 18:08:37 +0200

    reorder

--- src/Agent.cpp
+++ src/Agent.cpp
@@ -21,13 +21,17 @@ void ContactTracer::act(Session &session) {
     {
        int source = session.dequeueInfected();
        Tree* tree = Tree::createTree(session,source); // take node from the infected node and create tree
-       session.getGraph().BFS(session,tree); //built bfs tree from source
-       int node = tree->traceTree(); //check   - the node need to dis
+       int node = session.getGraph().BFS(session,tree)->traceTree();//built bfs tree from source and do tracetree on thr bfs
        session.removeEdges(session.getGraph(),node);
-       Virus *v = new Virus(node);
+
+
+       Virus *v = new Virus(node);// check - i think its not true// we need to delete it metar
+
+
        ContactTracer *cT = new ContactTracer();
        session.addAgent(*cT);
-       session.addAgent(*v);
+
+       session.addAgent(*v);//we need to deldet it to it hapend in act of virud
     }
 }
 
@@ -46,10 +50,11 @@ Virus::Virus(int nodeInd):Agent(), nodeInd(nodeInd) {};
 //methods
 void Virus::act(Session &session) {
      Tree* tree = Tree::createTree(session,nodeInd);
-     int indCarrrVirus=session.getGraph().BFS(session,tree)->getChildren().front()->getNode();//check about root of bfs
-     Virus *carryVirus = new Virus(indCarrrVirus);
+     int indCarrVirus=session.getGraph().BFS(session,tree)->getChildren().front()->getNode();//check about root of bfs
+     Virus *carryVirus = new Virus(indCarrVirus);
      session.addAgent(*carryVirus);// add our new carry virus to the agents list
      session.setStatus(nodeInd, 2);//change the status of nodeind from carry to sick
+     session.enqueueInfected(nodeInd);//add the sick node to the infected list-----------------------------/16/11
      session.setStatus(carryVirus->nodeInd,1);//change the new carry from health to carry
 }
 
